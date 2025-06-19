3cc482e0d763559f2e788529faab558558462513
devSJ05 <shardul.joshi05@outlook.com>
2025-06-19 19:33:46 +0530

    feat: creation of Password input component

--- react-machine-code-prep/src/App.jsx
+++ react-machine-code-prep/src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css';
 // import Accordion from './components/Accordion/Accordion';
 // import GuessTheNumber from './components/GuessTheNumber/GuessTheNumber';
-import Chips from './components/Chips/Chips';
+// import Chips from './components/Chips/Chips';
+import PasswordInput from './components/PasswordInput/PasswordInput';
 
 function App() {
   return (
     <>
       {/* <Accordion /> */}
       {/* <GuessTheNumber /> */}
-      <Chips />
+      {/* <Chips /> */}
+      <PasswordInput />
     </>
   );
 }
---
+++ react-machine-code-prep/src/components/PasswordInput/PasswordInput.tsx
@@ -0,0 +1,29 @@
+/*
+The requirement is to create a password input component in React that allows users to enter their passwords securely.
+Have a button beside the input field to toggle the visibility of the password.
+
+*/
+import { useState } from 'react';
+
+const PasswordInput = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <div className='container'>
+      <input
+        type={showPassword ? 'text' : 'password'}
+        placeholder='Enter your password'
+        className='input'
+      />
+      <button
+        type='button'
+        onClick={() => setShowPassword(!showPassword)}
+        className='button'
+      >
+        {showPassword ? 'Hide' : 'Show'}
+      </button>
+    </div>
+  );
+};
+
+export default PasswordInput;
